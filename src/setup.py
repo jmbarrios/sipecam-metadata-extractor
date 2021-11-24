f1c3118bfbf4182257475e1c46b5dd4f7c03a920
erick <palmoreck@gmail.com>
2021-11-24 11:48:55 -0600

    issue #1 taks2 progress

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,8 @@ setup(name="simex", version=0.1,
                           ],
       entry_points = {
           'console_scripts': [
-                              "list_of_files_to_extract_metadata=simex.list_of_files_to_extract_metadata:main",
+                              "list_of_files_to_extract_metadata = simex.list_of_files_to_extract_metadata:main",
+                              "extract_metadata_and_ingest_it = simex.extract_metadata_and_ingest_it:main"
                               ]
                      }
       )
