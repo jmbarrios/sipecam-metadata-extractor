530c1e442f57e033de349ad4b8a051e15c04ec40
erick <palmoreck@gmail.com>
2021-11-30 12:30:23 -0600

    stablishing format for logging

--- src/simex/__init__.py
+++ src/simex/__init__.py
@@ -1,8 +1,10 @@
 import logging
 
 def setup_logging(logs_filename):
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
     file_handler = logging.FileHandler(logs_filename, mode='a')
     file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logFormatter)
     logger = logging.getLogger()
     logger.setLevel(file_handler.level)
     logger.addHandler(file_handler)
