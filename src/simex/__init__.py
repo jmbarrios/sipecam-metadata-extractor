e846449da5489146749613af4694d47284aef067
erick <palmoreck@gmail.com>
2021-12-13 21:16:48 -0600

    making get_logger more general

--- src/simex/__init__.py
+++ src/simex/__init__.py
@@ -19,18 +19,19 @@ def setup_logging(logs_filename):
     logger.addHandler(file_handler)
     return logger
 
-def get_logger_for_writing_logs_to_file(input_file):
+def get_logger_for_writing_logs_to_file(input_file, temp_dirname):
     """
     Helper function to setup logging to level specified
     in setup_logging function.
     Args:
         input_file (str): basename of file that will be processed.
         It's used for creating file with logs.
+        temp_dirname (str): name of directory that will hold logs files.
     Returns:
         getLogger instance from logging module.
     """
     dirname_file = os.path.join(os.path.dirname(input_file),
-                                "temp_logs_simex_extract_metadata_and_ingest")
+                                temp_dirname)
     os.makedirs(dirname_file, exist_ok=True)
     logs_filename = os.path.join(dirname_file, "logs_" + \
                                  pathlib.Path(input_file).stem + ".txt") #trail extension
