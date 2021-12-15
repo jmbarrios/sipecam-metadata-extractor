9e9fafeedfeb1ec90c86cad5c1568fc14e6a7dcd
erick <palmoreck@gmail.com>
2021-12-15 14:18:48 -0600

    strings and cli new

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+cli_list = "list_of_files_and_subdirectories_to_extract_metadata = simex.list_of_files_and_subdirectories_to_extract_metadata:main"
+cli_extract_sn_coord = "extract_serial_number_and_coordinates_of_file = simex.extract_serial_number_and_coordinates_of_file:main"
+cli_extract_met_and_ing = "extract_metadata_and_ingest_it = simex.extract_metadata_and_ingest_it:main"
+
 setup(name="simex", version=0.1,
       description=u"Simplify extraction of metadata for SiPeCaM files",
       author="CONABIO",
@@ -11,8 +15,9 @@ setup(name="simex", version=0.1,
                           ],
       entry_points = {
           'console_scripts': [
-                              "list_of_files_and_subdirectories_to_extract_metadata = simex.list_of_files_and_subdirectories_to_extract_metadata:main",
-                              "extract_metadata_and_ingest_it = simex.extract_metadata_and_ingest_it:main"
+                              cli_list,
+                              cli_extract_sn_coord,
+                              cli_extract_met_and_ing
                               ]
                      }
       )
