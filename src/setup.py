2e584b3d4880ecb34dc09ff451cc3563c13bd91d
erick <palmoreck@gmail.com>
2022-02-08 16:21:14 -0600

    missing generate schema script and copy files to std dir funcitonality

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,7 @@ cli_list = "list_of_files_and_subdirectories_to_extract_metadata = simex.list_of
 cli_extract_sn_dt_coord = "extract_serial_number_datetime_and_coordinates_of_files = simex.extract_serial_number_datetime_and_coordinates_of_files:main"
 cli_extract_met_and_ing = "extract_metadata_and_ingest_it = simex.extract_metadata_and_ingest_it:main"
 cli_generate_sipecam_zendro_schema = "generate_sipecam_zendro_schema = simex.generate_sipecam_zendro_schema:main"
+cli_copy_files_to_standard_directory = "copy_files_to_standard_directory = simex.copy_files_to_standard_directory:main"
 
 setup(name="simex", version=0.1,
       description=u"Simplify extraction of metadata for SiPeCaM files",
@@ -21,7 +22,8 @@ setup(name="simex", version=0.1,
                               cli_list,
                               cli_extract_sn_dt_coord,
                               cli_extract_met_and_ing,
-                              cli_generate_sipecam_zendro_schema
+                              cli_generate_sipecam_zendro_schema,
+                              cli_copy_files_to_standard_directory
                               ]
                      }
       )
---
+++ src/simex/generate_sipecam_zendro_schema.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+
+from simex.utils import zendro
+
+def call_sgqlc_introspection_and_codegen():
+    
+    dir_with_schema = os.path.join(os.path.expanduser('~'),
+                                   "sipecam-metadata-extractor/src/simex")
+    os.makedirs(dir_with_schema, exist_ok=True)
+    
+    filename_schema = os.path.join(dir_with_schema,
+                                   "sipecam_zendro_schema")        
+    string = "".join(["\"Authorization: bearer ",
+                      zendro.get_token(),
+                      "\"",
+                      " ",
+                      zendro.get_zendro_url_for_gql(),
+                      " "])   
+    cmd1 = "".join(["python3 -m sgqlc.introspection --exclude-deprecated --exclude-description -H ", 
+                    string,
+                    filename_schema,
+                    ".json"])
+    cmd2 = "".join(["sgqlc-codegen schema ",
+                    filename_schema,
+                    ".json",
+                    " ",
+                    filename_schema,
+                    ".py"])
+    run_out_cmd1 = subprocess.run(cmd1,
+                          shell=True,
+                          capture_output=True)
+    print("Standard Error sgqlc.introspection %s" % run_out_cmd1.stderr)
+    print("Standard Output sgqlc.introspection %s" %run_out_cmd1.stdout)
+    run_out_cmd2 = subprocess.run(cmd2,
+                          shell=True,                              
+                          capture_output=True)
+    print("Standard Error sgqlc-codegen %s" % run_out_cmd2.stderr)
+    print("Standard Output sgqlc-codegen %s" %run_out_cmd2.stdout)
+    
+def main():
+    call_sgqlc_introspection_and_codegen()
