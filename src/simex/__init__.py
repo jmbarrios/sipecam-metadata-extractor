77ea3f447dc2b7da8202718bfcfc11f8be11f6f1
erick <palmoreck@gmail.com>
2022-02-08 16:17:34 -0600

    changing name of arg as is filename and not dir

--- src/simex/__init__.py
+++ src/simex/__init__.py
@@ -28,21 +28,21 @@ def setup_logging(logs_filename):
     logger.addHandler(file_handler)
     return logger
 
-def get_logger_for_writing_logs_to_file(input_file_or_dir, temp_dirname):
+def get_logger_for_writing_logs_to_file(input_file_or_dir, filename_for_logs):
     """
     Helper function to setup logging to level specified
     in setup_logging function.
     Args:
         input_file_or_dir (str): file or directory that will be processed (includes path of file).
         It's used for creating file with logs.
-        temp_dirname (str): name of directory that will hold logs files.
+        filename_for_logs (str): name of file that will hold logs files.
     Returns:
         getLogger instance from logging module.
     """
     input_file_or_dir_path_lib = pathlib.Path(input_file_or_dir)
     if input_file_or_dir_path_lib.suffix: #is file
         dirname_file = os.path.join(os.path.dirname(input_file_or_dir),
-                                    temp_dirname)
+                                    filename_for_logs)
         os.makedirs(dirname_file, exist_ok=True)
         logs_filename = os.path.join(dirname_file,
                                      input_file_or_dir_path_lib.stem + \
@@ -50,7 +50,7 @@ def get_logger_for_writing_logs_to_file(input_file_or_dir, temp_dirname):
                                      ".logs")
     else: #is directory
         dirname_file = os.path.join(input_file_or_dir,
-                                    temp_dirname)
+                                    filename_for_logs)
         logs_filename = dirname_file + ".logs"
     pathlib.Path(logs_filename).unlink(missing_ok=True) #remove in case it exists
     return setup_logging(logs_filename)
