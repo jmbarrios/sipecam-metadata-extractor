d8ec6d9b3a2854b4934a6c427c3bc8d00bc972f1
erick <palmoreck@gmail.com>
2022-02-14 17:35:02 -0600

    using find_packages func in setup for utils module and others of simex

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ cli_copy_files_to_standard_directory = "copy_files_to_standard_directory = simex
 setup(name="simex", version=0.1,
       description=u"Simplify extraction of metadata for SiPeCaM files",
       author="CONABIO",
-      packages=['simex'],
+      packages=find_packages(),
       install_requires = [
                           "guano",
                           "pillow",
