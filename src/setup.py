30d735c872888eda8ccc361a29157a674cf00c9e
erick <palmoreck@gmail.com>
2022-02-25 12:12:57 -0600

    PyExifTool

--- src/setup.py
+++ src/setup.py
@@ -6,16 +6,18 @@ cli_extract_met_and_ing = "extract_metadata_and_ingest_it = simex.extract_metada
 cli_generate_sipecam_zendro_schema = "generate_sipecam_zendro_schema = simex.generate_sipecam_zendro_schema:main"
 cli_copy_files_to_standard_directory = "copy_files_to_standard_directory = simex.copy_files_to_standard_directory:main"
 
+#"guano",
+#,
+#"hachoir",
+
 setup(name="simex", version=0.1,
       description=u"Simplify extraction of metadata for SiPeCaM files",
       author="CONABIO",
       packages=find_packages(),
-      install_requires = [
-                          "guano",
-                          "pillow",
-                          "hachoir",
+      install_requires = ["pillow",
                           "python-dotenv",
-                          "sgqlc"
+                          "sgqlc",
+                          "PyExifTool"
                           ],
       entry_points = {
           'console_scripts': [
