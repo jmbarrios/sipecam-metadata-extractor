44ef2aecf2b7cb16a3433b6193a775dcdc33ca4d
erick <palmoreck@gmail.com>
2022-03-01 09:22:30 -0600

    including hachoir and deleting pillow

--- src/setup.py
+++ src/setup.py
@@ -6,17 +6,13 @@ cli_extract_met_and_ing = "extract_metadata_and_ingest_it = simex.extract_metada
 cli_generate_sipecam_zendro_schema = "generate_sipecam_zendro_schema = simex.generate_sipecam_zendro_schema:main"
 cli_copy_files_to_standard_directory = "copy_files_to_standard_directory = simex.copy_files_to_standard_directory:main"
 
-#"guano",
-#,
-#"hachoir",
-
 setup(name="simex", version=0.1,
       description=u"Simplify extraction of metadata for SiPeCaM files",
       author="CONABIO",
       packages=find_packages(),
-      install_requires = ["pillow",
-                          "python-dotenv",
+      install_requires = ["python-dotenv",
                           "sgqlc",
+                          "hachoir",
                           "PyExifTool"
                           ],
       entry_points = {
