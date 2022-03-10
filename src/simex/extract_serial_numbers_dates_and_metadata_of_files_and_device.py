84755e7acdad275109898b90e665c8acb82d5ce6
erick <palmoreck@gmail.com>
2022-03-10 11:18:54 -0600

    support for finding audio empty files

--- src/simex/extract_serial_numbers_dates_and_metadata_of_files_and_device.py
+++ src/simex/extract_serial_numbers_dates_and_metadata_of_files_and_device.py
@@ -7,6 +7,7 @@ from operator import itemgetter
 import datetime
 from multiprocessing import Pool
 from functools import reduce
+import subprocess
 
 from simex import get_logger_for_writing_logs_to_file
 from simex import SUFFIXES_SIPECAM, SUFFIXES_SIPECAM_AUDIO, \
@@ -16,6 +17,28 @@ from simex import read_metadata_image, read_metadata_video, read_metadata_audio
 
 SUFFIXES_AUDIO_IMAGES = SUFFIXES_SIPECAM_AUDIO + SUFFIXES_SIPECAM_IMAGES
 
+def check_if_audio_file_is_empty_and_move_it(filename):
+    """
+    Auxiliary function to check if exiftool -a -j filename returns: "Error": "File is empty"
+    """
+    f_pathlib = pathlib.Path(filename)
+    cmd = "".join(["exiftool -a -j ", "\"", filename, "\""])
+    run_out_cmd = subprocess.run(cmd,
+                                 shell=True,
+                                 capture_output=True)
+    run_out_cmd_stdout_empty = run_out_cmd.stdout.find(b"empty")
+    if run_out_cmd_stdout_empty != -1:
+        dir_pathlib = f_pathlib.parent
+        dir_error = os.path.join(dir_pathlib,
+                                 "error")
+        os.makedirs(dir_error, exist_ok=True)
+        dst_f_error = os.path.join(dir_error,
+                                   f_pathlib.name)
+        f_pathlib.rename(dst_f_error)
+    else:
+        dst_f_error = ""
+    return (run_out_cmd_stdout_empty, dst_f_error)
+
 def extract_metadata(filename):
     """
     Helper function to extract date and metadata of file.
@@ -24,8 +47,13 @@ def extract_metadata(filename):
     f_pathlib = pathlib.Path(filename)
     date_of_file = ""
     if f_pathlib.suffix in SUFFIXES_SIPECAM_AUDIO:
-        date_of_file = read_metadata_audio.extract_date(filename)
-        tuple_metadata_of_file = (filename,read_metadata_audio.get_metadata_of_file(filename))
+        res_empty, new_dst_for_filename = check_if_audio_file_is_empty_and_move_it(filename)
+        if res_empty == -1:
+            date_of_file = read_metadata_audio.extract_date(filename)
+            tuple_metadata_of_file = (filename,read_metadata_audio.get_metadata_of_file(filename))
+        else: #audio empty file
+            date_of_file = ""
+            tuple_metadata_of_file = (filename, new_dst_for_filename)
     else:
         if f_pathlib.suffix in SUFFIXES_SIPECAM_IMAGES:
             date_of_file = read_metadata_image.extract_date(filename)
@@ -109,23 +137,49 @@ def main():
         while not_success:
             try:
                 filename = next(iterator)
+                f_pathlib = pathlib.Path(filename)
                 logger.info("extraction of metadata of device from %s" % filename)
-                res_extract_metadata_of_device = extract_metadata_of_device(filename)
-                serial_number_file = res_extract_metadata_of_device["SerialNumber"]
-                if serial_number_file:
-                    logger.info("SUCCESSFUL extraction of serial number of %s" % filename)
-                    not_success = False
-                    d_output["MetadataDevice"] = res_extract_metadata_of_device
-                if len(d_output["MetadataDevice"].keys()) < 1:
-                    logger.info("FAILED extraction of serial number of files in dir %s" % input_dir)
+                if f_pathlib.suffix in SUFFIXES_SIPECAM_AUDIO:
+                    res_empty, new_dst_for_filename = check_if_audio_file_is_empty_and_move_it(filename)
+                else:
+                    if f_pathlib.suffix in SUFFIXES_SIPECAM_IMAGES or SUFFIXES_SIPECAM_VIDEO: #for images and videos, no checking of empty file
+                        res_empty = -1
+                        new_dst_for_filename = ""
+                if res_empty == -1:
+                    audio_empty_file = False
+                    res_extract_metadata_of_device = extract_metadata_of_device(filename)
+                    serial_number_file = res_extract_metadata_of_device["SerialNumber"]
+                    if serial_number_file:
+                        logger.info("SUCCESSFUL extraction of serial number of %s" % filename)
+                        not_success = False
+                        d_output["MetadataDevice"] = res_extract_metadata_of_device
+                else:#audio empty file
+                    audio_empty_file = True
+                    if new_dst_for_filename:
+                        logger.info("FAILED, found empty content when using exiftool in file %s, was moved to %s" % (filename,
+                                                                                                             new_dst_for_filename)
+                                   )
             except Exception as e:
                 logger.info(e)
-                logger.info("there were no audios nor images found in dir: %s, serial number can not be retrieved from " % input_dir)
+                logger.info("FAILED, there were no audios nor images found in dir: %s or reached last element in iterator" % input_dir)
                 not_success = False
-        #as videos have no coordinates in d_output["GPSFile"] for images they will be saved
-        f_pathlib = pathlib.Path(filename)
-        if f_pathlib.suffix in SUFFIXES_SIPECAM_IMAGES:
-            d_output["GPSFile"] = read_metadata_image.extract_gps(filename)
+        try:
+            filename = next(iterator)
+            reached_last_item_in_iterator = False
+        except Exception as e:
+            logger.info(e)
+            reached_last_item_in_iterator = True
+        if audio_empty_file and reached_last_item_in_iterator:
+            return -1
+        else:
+            if serial_number_file:
+                #as videos have no coordinates in d_output["GPSFile"] for images they will be saved
+                if f_pathlib.suffix in SUFFIXES_SIPECAM_IMAGES:
+                    d_output["GPSFile"] = read_metadata_image.extract_gps(filename)
+                return 0
+            else: #couldn't retrieve serial number
+                logger.info("FAILED retrieving serial number of dir %s" % input_dir)
+                return -1
 
     def extract_metadata_of_files(input_dir,
                                   d_output,
@@ -146,6 +200,11 @@ def main():
                     if not date_file:
                         logger.info("FAILED extraction of date of file %s" % filename)
                         logger.info("returning empty date")
+                        filename, new_dst_for_filename = t[1] #t[1] tuple with filename and possible new dst of file as 1st, 2nd elements resp
+                        if new_dst_for_filename:
+                            logger.info("found empty content when using exiftool in file %s, was moved to %s" % (filename,
+                                                                                                                 new_dst_for_filename)
+                                       )
                     else:
                         logger.info("SUCCESSFUL extraction of date of %s" % filename)
                         d_output["Dates"][filename] = date_file
@@ -213,22 +272,24 @@ def main():
         d_output_dates_keys = d_output["Dates"].keys()
         if len(d_output_dates_keys) >= 2:
             extract_first_last_dates_and_difference()
-
-    with open(output_filename, "w") as dst:
-        dict_output = {}
-        dict_gps_for_videos = {}
-        extract_metadata_of_device_from_files(input_directory,
-                                              dict_output)
-        try:
-            dict_gps_for_videos = dict_output["GPSFile"]
-        except Exception as e:
-            logger.info("GPSFile key not found for videos, files from directory are of audio")
-        extract_metadata_of_files(input_directory,
-                                  dict_output,
-                                  dict_gps_for_videos)
-        try:
-            del dict_output["GPSFile"]
-        except Exception as e:
-            logger.info("GPSFile key not found for videos, files from directory are of audio")
-        json.dump(dict_output, dst)
+    boolean_value = extract_metadata_of_device_from_files(input_directory,
+                                                          dict_output)
+    if boolean_value == -1:
+        logger.info("Error when reading files from dir %s" % input_directory)
+    else:
+        with open(output_filename, "w") as dst:
+            dict_output = {}
+            dict_gps_for_videos = {}
+            try:
+                dict_gps_for_videos = dict_output["GPSFile"]
+            except Exception as e:
+                logger.info("GPSFile key not found for videos, files from directory are of audio")
+            extract_metadata_of_files(input_directory,
+                                      dict_output,
+                                      dict_gps_for_videos)
+            try:
+                del dict_output["GPSFile"]
+            except Exception as e:
+                logger.info("GPSFile key not found for videos, files from directory are of audio")
+            json.dump(dict_output, dst)
 
