ec412916480083255d682b4b185798f6ee8d373a
erick <palmoreck@gmail.com>
2022-03-15 19:58:33 -0600

    cli for extend metadata of audio files

--- src/setup.py
+++ src/setup.py
@@ -6,6 +6,7 @@ cli_extract_met_and_ing = "extract_metadata_and_ingest_it = simex.extract_metada
 cli_generate_sipecam_zendro_schema = "generate_sipecam_zendro_schema = simex.generate_sipecam_zendro_schema:main"
 cli_move_files_to_standard_directory = "move_files_to_standard_directory = simex.move_files_to_standard_directory:main"
 cli_check_if_there_are_audio_empty_files_in_dir_and_move_them = "check_if_there_are_audio_empty_files_in_dir_and_move_them = simex.check_if_there_are_audio_empty_files_in_dir_and_move_them:main"
+cli_extend_metadata_of_audio_files = "extend_metadata_of_audio_files = simex.extend_metadata_of_audio_files:main"
 
 setup(name="simex", version=0.1,
       description=u"Simplify extraction of metadata for SiPeCaM files",
@@ -23,7 +24,8 @@ setup(name="simex", version=0.1,
                               cli_extract_met_and_ing,
                               cli_generate_sipecam_zendro_schema,
                               cli_move_files_to_standard_directory,
-                              cli_check_if_there_are_audio_empty_files_in_dir_and_move_them
+                              cli_check_if_there_are_audio_empty_files_in_dir_and_move_them,
+                              cli_extend_metadata_of_audio_files
                               ]
                      }
       )
