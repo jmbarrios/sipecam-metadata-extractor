1bf447f7b7c4dacf1150418ce91a1eb21fd88b60
erick <palmoreck@gmail.com>
2022-03-30 20:16:04 -0600

    functionality to extend metadata of files, validation of coordinates and cumulus centroid lat long

--- src/setup.py
+++ src/setup.py
@@ -7,6 +7,7 @@ cli_generate_sipecam_zendro_schema = "generate_sipecam_zendro_schema = simex.gen
 cli_move_files_to_standard_directory = "move_files_to_standard_directory = simex.move_files_to_standard_directory:main"
 cli_check_if_there_are_audio_empty_files_in_dir_and_move_them = "check_if_there_are_audio_empty_files_in_dir_and_move_them = simex.check_if_there_are_audio_empty_files_in_dir_and_move_them:main"
 cli_extend_metadata_of_audio_files = "extend_metadata_of_audio_files = simex.extend_metadata_of_audio_files:main"
+cli_extend_metadata_of_files = "extend_metadata_of_files = simex.extend_metadata_of_files:main"
 
 setup(name="simex", version=0.1,
       description=u"Simplify extraction of metadata for SiPeCaM files",
@@ -15,7 +16,8 @@ setup(name="simex", version=0.1,
       install_requires = ["python-dotenv==0.19.2",
                           "sgqlc==15.0",
                           "hachoir==3.1.2",
-                          "PyExifTool==0.4.13 "
+                          "PyExifTool==0.4.13",
+                          "shapely"
                           ],
       entry_points = {
           'console_scripts': [
@@ -25,7 +27,8 @@ setup(name="simex", version=0.1,
                               cli_generate_sipecam_zendro_schema,
                               cli_move_files_to_standard_directory,
                               cli_check_if_there_are_audio_empty_files_in_dir_and_move_them,
-                              cli_extend_metadata_of_audio_files
+                              cli_extend_metadata_of_audio_files,
+                              cli_extend_metadata_of_files
                               ]
                      }
       )
