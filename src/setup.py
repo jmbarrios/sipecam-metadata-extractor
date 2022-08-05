92c2c69351b4a226e1e02d8c2cfcd2f261020a4b
erick <palmoreck@hotmail.com>
2022-08-05 11:43:22 -0500

    cli to check if files have diff serial number in same dir

--- src/setup.py
+++ src/setup.py
@@ -8,6 +8,7 @@ cli_check_if_there_are_audio_empty_files_in_dir_and_move_them = "check_if_there_
 cli_extend_metadata_of_audio_files = "extend_metadata_of_audio_files = simex.extend_metadata_of_audio_files:main"
 cli_extend_metadata_of_files = "extend_metadata_of_files = simex.extend_metadata_of_files:main"
 cli_extend_metadata_of_video_files = "extend_metadata_of_video_files = simex.extend_metadata_of_video_files:main"
+cli_check_if_files_in_dir_are_from_different_serial_numbers_and_move_them = "check_if_files_in_dir_are_from_different_serial_numbers_and_move_them = simex.check_if_files_in_dir_are_from_different_serial_numbers_and_move_them:main"
 
 setup(name="simex", version=0.1,
       description=u"Simplify extraction of metadata for SiPeCaM files",
@@ -28,7 +29,8 @@ setup(name="simex", version=0.1,
                               cli_check_if_there_are_audio_empty_files_in_dir_and_move_them,
                               cli_extend_metadata_of_audio_files,
                               cli_extend_metadata_of_files,
-                              cli_extend_metadata_of_video_files
+                              cli_extend_metadata_of_video_files,
+                              cli_check_if_files_in_dir_are_from_different_serial_numbers_and_move_them
                               ]
                      }
       )
