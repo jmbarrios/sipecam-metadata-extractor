b61e02d8dccb03816167cc27df2c0714c13def2d
Juan M Barrios <j.m.barrios@gmail.com>
2022-09-27 14:05:01 -0500

    (chore) improve readability on `list_of_files...`
    a more readable code was added to
    `list_of_files_and_subdirectories_to_extract_metadata.py` file.
    Removed `os` dependency and updated to `pathlib`.

--- src/simex/list_of_files_and_subdirectories_to_extract_metadata.py
+++ src/simex/list_of_files_and_subdirectories_to_extract_metadata.py
@@ -1,10 +1,8 @@
-import os
-import datetime
 import argparse
+import datetime
+from pathlib import Path
 
 from simex import SUFFIXES_SIPECAM
-from simex.utils.directories_and_files import multiple_file_types
-
 
 def arguments_parse():
     help = """
@@ -31,27 +29,22 @@ list_of_files_and_subdirectories_to_extract_metadata --input_directory /input_di
 
 def main():
     args = arguments_parse()
-    input_directory = args.input_directory
-    shared_volume = "/shared_volume"
-    os.makedirs(shared_volume, exist_ok=True)
-    sipecam_subdirectories = os.path.join(shared_volume,
-                                          "sipecam_subdirectories_" + \
-                                          datetime.date.today().strftime("%d-%m-%Y") + \
-                                          ".txt")
-    sipecam_files_to_extract_metadata = os.path.join(shared_volume,
-                                        "sipecam_files_to_extract_metadata_from_" + \
-                                        datetime.date.today().strftime("%d-%m-%Y") + \
-                                        ".txt")
+    input_directory = Path(args.input_directory)
+    shared_volume = Path("/shared_volume")
+    shared_volume.mkdir(parents=True, exist_ok=True)
+    date_str = datetime.date.today().strftime("%d-%m-%Y")
+    sipecam_subdirectories = ( shared_volume /
+            f"sipecam_subdirectories_{date_str}.txt" )
+    sipecam_files_to_extract_metadata = ( shared_volume /
+            f"sipecam_files_to_extract_metadata_from_{date_str}.txt" )
 
-    list_of_subdirectories = []
+    list_of_files = [f.resolve() for f in input_directory.glob('**/*')
+                     if f.suffix in SUFFIXES_SIPECAM]
+    list_of_subdirectories = { f.parent for f in list_of_files }
 
-    with open(sipecam_files_to_extract_metadata, "w+") as file:
-        with open(sipecam_subdirectories, "w+") as subdirectory:
-            for f in multiple_file_types(input_directory, SUFFIXES_SIPECAM,
-                                         recursive=True):
-                file.write(f + "\n")
-                dirname_f = os.path.dirname(f)
-                if dirname_f not in list_of_subdirectories:
-                    subdirectory.write(dirname_f + "\n")
-                    list_of_subdirectories.append(dirname_f)
+    with sipecam_files_to_extract_metadata.open(mode='w+') as f:
+        f.writelines([f'{str(i)}\n' for i in list_of_files])
+
+    with sipecam_subdirectories.open(mode='w+') as f:
+        f.writelines([f'{str(i)}\n' for i in list_of_subdirectories])
 
